4e86ebc8e2cac547450b619aafdbc11f354f52fa
abdelrahman725 <bedokhaled66@gmail.com>
2022-05-18 03:28:49 +0200

    Select animes in Contribution Component has been styled using react-select package

--- app/src/Components/Contripution.js
+++ app/src/Components/Contripution.js
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect,useRef } from "react"
 import { ServerContext } from "../App"
 import getCookie from "../GetCookie"
+import Select from 'react-select'
 
 const Contripution = () => {
 
@@ -8,14 +9,19 @@ const Contripution = () => {
   const CsrfToken = getCookie('csrftoken')
   const [animesoptions,setanimesoptions] = useState()
   
-  
+
   //load all animes:
 
    const GetAllAnimes =async ()=>
    {
      const res = await fetch(`${server}/home/animesoptions`)
      const animes  = await res.json()
-     setanimesoptions(animes)
+     const anime_array = []
+      animes.map((anime) => 
+      anime_array.push({value:anime.id,label:anime.anime_name})
+      )
+
+     setanimesoptions(anime_array)
    }
 
   
@@ -28,16 +34,36 @@ const Contripution = () => {
   })
 
   const[anime,setanime]= useState()
-
-  const firstinput = useRef(null)
+  
+  const input_2 = useRef(null)
+  const input_3 = useRef(null)
+  const input_4 = useRef(null)
+  const input_5 = useRef(null)
+  const submit_btn = useRef(null)
+  const select_animes = useRef(null)
+  
+  const handleselect=(e)=> {setanime(e.value)}
 
   const handlechange = (e)=>
-  {
-    const {name,value} = e.target
+  {    
+
+    const{name,value} = e.target
+
+    if (value[value.length-1]==="\n")
+    {
+      name==="question"&&input_2.current.focus()
+      name==="rightanswer"&&input_3.current.focus()
+      name==="choice1"&&input_4.current.focus()
+      name==="choice2"&&input_5.current.focus()
+      name==="choice3"&&submit_btn.current.focus()
+      return 
+    }
+      // e.nativeEvent.inputType === "insertLineBreak" 
+    
     setQuestion(prev => ({...prev, [name]: value}))
   }
 
-  const handleselect=(e)=> {setanime(e.target.value)}
+ 
 
   const HandleSubmision = (e)=>
   {
@@ -69,13 +95,20 @@ const Contripution = () => {
       }
       
 
-     anime&& anime!=="choose an anime" && SendContribution()
+
+      if (anime!==undefined){
+        SendContribution()
+        document.activeElement.blur() 
+      }
+
+      else{
+        select_animes.current.focus()  
+      }
   }
 
+ 
 
-  
   useEffect(()=>{ 
-    // firstinput.current.focus()
     GetAllAnimes()
   },[])
 
@@ -88,46 +121,45 @@ const Contripution = () => {
       <form onSubmit={HandleSubmision} >
         
         <div className="form_elements">
-       
 
-          <select onChange={handleselect} id="pet-select"    >
-            <option>choose an anime</option>        
-            
-              {animesoptions&&animesoptions.map((anime,index)=>(
-              <option value={anime.id} key={index}>{anime.anime_name}</option>))}
-          </select>
-      <br /><br />
-
+        <Select options={animesoptions} className="select_animes"  placeholder="select anime" 
+        onChange={handleselect} ref={select_animes}/>
+        
+        <br /><br />
 
 
      <textarea name="question" cols="30" rows="3" typeof="text" value={Question.question}
-   
-       onChange={handlechange} required placeholder="what is the question?">
+       onChange={handlechange} 
+        required placeholder="what is the question?" >
        </textarea><br />
 
-     <textarea name="rightanswer" cols="30" rows="3" typeof="text" value={Question.rightanswer}
-       onChange={handlechange} required placeholder="right answer">
+     <textarea name="rightanswer" cols="30" rows="3" typeof="text" 
+     value={Question.rightanswer} ref={input_2}
+     onChange={handlechange} required placeholder="right answer">
        </textarea><br />
   
 
-     <h3>choices  <span>(wrong answers)</span></h3>
+     <h3>choices<span> (wrong answers)</span></h3>
 
    
 
-       <textarea name="choice1" cols="30" rows="3" typeof="text" value={Question.choice1}
-       onChange={handlechange} required placeholder="choice 1">
+       <textarea name="choice1" cols="30" rows="3" typeof="text" value={Question.choice1} ref={input_3}
+        onChange={handlechange} 
+        required placeholder="choice 1">
        </textarea><br />
     
-       <textarea name="choice2" cols="30" rows="3" typeof="text" value={Question.choice2}
-       onChange={handlechange} required placeholder="choice 2">
+       <textarea name="choice2" cols="30" rows="3" typeof="text" value={Question.choice2}  ref={input_4}
+        onChange={handlechange} 
+       required placeholder="choice 2">
        </textarea><br />
     
-       <textarea name="choice3" cols="30" rows="3" typeof="text" value={Question.choice3}
-       onChange={handlechange} required placeholder="choice 3">
+       <textarea name="choice3" cols="30" rows="3" typeof="text" value={Question.choice3}  ref={input_5}
+        onChange={handlechange} 
+       required placeholder="choice 3">
        </textarea><br />
 
       <br />
-      <button type="submit">submit question </button>
+      <button type="submit" ref={submit_btn}>submit question </button>
 
 
       </div>
